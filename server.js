c5068ab24014c80fad42bdb7b45987e3a1358ac4
blair49 <31095601+blair49@users.noreply.github.com>
2018-02-19 16:24:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,10 +17,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
 app.get('/article1', function (req, res) {
   res.send('Article 1 requested and will be served here');
 });
